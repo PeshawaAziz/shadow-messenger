884a0707261e68aba8945b440559c700875e0454
Peshawa Aziz <modern3p2017@gmail.com>
2021-07-28 23:39:55 +0430

    Add input message functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 const CHAT_LIST = document.querySelector(".chat-list");
+const CHAT_WINDOW = document.querySelector(".chat-window");
+const NEW_MESSAGE_FORM = document.querySelector(".new-message-form");
 
 const CHATROOM = new Chatroom("general", "dudeNEW");
 const CHAT_UI = new ChatUI(CHAT_LIST);
@@ -6,3 +8,21 @@ const CHAT_UI = new ChatUI(CHAT_LIST);
 CHATROOM.getChats((data) => {
     CHAT_UI.render(data);
 });
+
+NEW_MESSAGE_FORM.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const MESSAGE = NEW_MESSAGE_FORM.message.value.trim();
+    CHATROOM.addChat(MESSAGE)
+        .then(() => {
+            updateScroll();
+            NEW_MESSAGE_FORM.reset();
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
+
+function updateScroll() {
+    CHAT_WINDOW.scrollTop = CHAT_WINDOW.scrollHeight;
+}
