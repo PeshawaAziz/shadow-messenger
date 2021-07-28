e5dab28c17e4efba7ad37492c033be5cd4f12889
Peshawa Aziz <modern3p2017@gmail.com>
2021-07-28 22:11:52 +0430

    Add realtime listener for updating the data with every change

--- js/chat.js
+++ js/chat.js
@@ -4,6 +4,7 @@ class Chatroom {
         this.room = room;
         this.username = username;
         this.chats = DATABASE.collection("chats");
+        this.unsub;
     }
     async addChat(message) {
         const NOW = new Date();
@@ -16,11 +17,28 @@ class Chatroom {
         const RESPONSE = await this.chats.add(CHAT);
         return RESPONSE;
     }
+    // Realtime listener (Getting the data with every change)
+    getChats(callback) {
+        this.unsub = this.chats
+            .where("room", "==", this.room)
+            .orderBy("time")
+            .onSnapshot((snapshot) => {
+                snapshot.docChanges().forEach((change) => {
+                    if (change.type == "added") {
+                        // Updating the UI
+                        callback(change.doc.data());
+                    }
+                });
+            });
+    }
+    updateName(newName) {
+        this.username = newName;
+    }
+    // Updating the room based on the input
+    updateRoom(newRoom) {
+        this.room = newRoom;
+        if (this.unsub) {
+            this.unsub();
+        }
+    }
 }
-
-const CHATROOM = new Chatroom("general", "dude1");
-CHATROOM.addChat("Hello, WOW!")
-    .then(() => {})
-    .catch((error) => {
-        console.log(error);
-    });
